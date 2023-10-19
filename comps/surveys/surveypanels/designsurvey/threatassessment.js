30782fad97ff49c706a731482b39453942492976
sub-ra <87712870+sub-ra@users.noreply.github.com>
2023-10-19 16:18:18 +0200

    Update threatassessment.js

--- comps/surveys/surveypanels/designsurvey/threatassessment.js
+++ comps/surveys/surveypanels/designsurvey/threatassessment.js
@@ -1,183 +1,185 @@
-const threatassessment = () => {return ({
-    "type": "panel",
-    "name": "Threat Assessment",
-    "elements": [
-      {
-        "type": "radiogroup",
-        "id": "71c02652a9ba4b10a0cc0179c7ce869f",
-        "title": "Do you classify applications according to business risk based on a simple and predefined set of questions?",
-        "titleLocation": "left",
-        "description": "An agreed-upon risk classification exists",
-        "choices": [
-          {
-            "value": 0,
-            "weight": 1,
-            "text": "No"
-          },
-          {
-            "value": 0.25,
-            "weight": 1,
-            "text": "Yes, some of them"
-          },
-          {
-            "value": 0.5,
-            "weight": 1,
-            "text": "Yes, at least half of them"
-          },
-          {
-            "value": 1,
-            "weight": 1,
-            "text": "Yes, most or all of them"
-          }
-        ]
-      },
-      {
-        "type": "radiogroup",
-        "id": "897306b66f16454eab7b5a2355d31c11",
-        "title": "Do you use centralized and quantified application risk profiles to evaluate business risk?",
-        "titleLocation": "left",
-        "description": "The application risk profile is in line with the organizational risk standard",
-        "choices": [
-          {
-            "value": 0,
-            "weight": 1,
-            "text": "No"
-          },
-          {
-            "value": 0.25,
-            "weight": 1,
-            "text": "Yes, for some applications"
-          },
-          {
-            "value": 0.5,
-            "weight": 1,
-            "text": "Yes, for at least half of the applications"
-          },
-          {
-            "value": 1,
-            "weight": 1,
-            "text": "Yes, for most or all of the applications"
-          }
-        ]
-      },
-      {
-        "type": "radiogroup",
-        "id": "7e541611f3c749f285ac27f0a9ba7d55",
-        "title": "Do you regularly review and update the risk profiles for your applications?",
-        "titleLocation": "left",
-        "description": "The organizational risk standard considers historical feedback to improve the evaluation method",
-        "choices": [
-          {
-            "value": 0,
-            "weight": 1,
-            "text": "No"
-          },
-          {
-            "value": 0.25,
-            "weight": 1,
-            "text": "Yes, sporadically"
-          },
-          {
-            "value": 0.5,
-            "weight": 1,
-            "text": "Yes, upon change of the application"
-          },
-          {
-            "value": 1,
-            "weight": 1,
-            "text": "Yes, at least annually"
-          }
-        ]
-      },
-      {
-        "type": "radiogroup",
-        "id": "e9dcf4f79e2e487fb74df5e15a14a91b",
-        "title": "Do you identify and manage architectural design flaws with threat modeling?",
-        "titleLocation": "left",
-        "description": "You perform threat modeling for high-risk applications",
-        "choices": [
-          {
-            "value": 0,
-            "weight": 1,
-            "text": "No"
-          },
-          {
-            "value": 0.25,
-            "weight": 1,
-            "text": "Yes, some of them"
-          },
-          {
-            "value": 0.5,
-            "weight": 1,
-            "text": "Yes, at least half of them"
-          },
-          {
-            "value": 1,
-            "weight": 1,
-            "text": "Yes, most or all of them"
-          }
-        ]
-      },
-      {
-        "type": "radiogroup",
-        "id": "42cfabd13db34fd0b35e92af917eb1b8",
-        "title": "Do you use a standard methodology, aligned on your application risk levels?",
-        "titleLocation": "left",
-        "description": "You train your architects, security champions, and other stakeholders on how to do practical threat modeling",
-        "choices": [
-          {
-            "value": 0,
-            "weight": 1,
-            "text": "No"
-          },
-          {
-            "value": 0.25,
-            "weight": 1,
-            "text": "Yes, for some applications"
-          },
-          {
-            "value": 0.5,
-            "weight": 1,
-            "text": "Yes, for at least half of the applications"
-          },
-          {
-            "value": 1,
-            "weight": 1,
-            "text": "Yes, for most or all of the applications"
-          }
-        ]
-      },
-      {
-        "type": "radiogroup",
-        "id": "cd639e5458ca4f60be60bc9d47314648",
-        "title": "Do you regularly review and update the threat modeling methodology for your applications?",
-        "titleLocation": "left",
-        "description": "The threat model methodology considers historical feedback for improvement",
-        "choices": [
-          {
-            "value": 0,
-            "weight": 1,
-            "text": "No"
-          },
-          {
-            "value": 0.25,
-            "weight": 1,
-            "text": "Yes, but review is ad-hoc"
-          },
-          {
-            "value": 0.5,
-            "weight": 1,
-            "text": "Yes, we review it at regular times"
-          },
-          {
-            "value": 1,
-            "weight": 1,
-            "text": "Yes, we review it at least annually"
-          }
-        ]
-      }
-    ],
-    "title": "Threat Assessment",
-    "state": "expanded"
-  });}
+const threatassessment = () => {
+    return ({
+        "type": "panel",
+        "name": "Bedrohungsbeurteilung",
+        "elements": [
+            {
+                "type": "radiogroup",
+                "id": "71c02652a9ba4b10a0cc0179c7ce869f",
+                "title": "Klassifizieren Sie Anwendungen nach Geschäftsrisiken anhand eines einfachen und vordefinierten Fragenkatalogs?",
+                "titleLocation": "left",
+                "description": "Eine vereinbarte Risikoklassifikation existiert",
+                "choices": [
+                    {
+                        "value": 0,
+                        "weight": 1,
+                        "text": "Nein"
+                    },
+                    {
+                        "value": 0.25,
+                        "weight": 1,
+                        "text": "Ja, einige davon"
+                    },
+                    {
+                        "value": 0.5,
+                        "weight": 1,
+                        "text": "Ja, mindestens die Hälfte davon"
+                    },
+                    {
+                        "value": 1,
+                        "weight": 1,
+                        "text": "Ja, die meisten oder alle davon"
+                    }
+                ]
+            },
+            {
+                "type": "radiogroup",
+                "id": "897306b66f16454eab7b5a2355d31c11",
+                "title": "Verwenden Sie zentralisierte und quantifizierte Anwendungsrisikoprofile zur Bewertung des Geschäftsrisikos?",
+                "titleLocation": "left",
+                "description": "Das Anwendungsrisikoprofil entspricht dem organisatorischen Risikostandard",
+                "choices": [
+                    {
+                        "value": 0,
+                        "weight": 1,
+                        "text": "Nein"
+                    },
+                    {
+                        "value": 0.25,
+                        "weight": 1,
+                        "text": "Ja, für einige Anwendungen"
+                    },
+                    {
+                        "value": 0.5,
+                        "weight": 1,
+                        "text": "Ja, für mindestens die Hälfte der Anwendungen"
+                    },
+                    {
+                        "value": 1,
+                        "weight": 1,
+                        "text": "Ja, für die meisten oder alle Anwendungen"
+                    }
+                ]
+            },
+            {
+                "type": "radiogroup",
+                "id": "7e541611f3c749f285ac27f0a9ba7d55",
+                "title": "Überprüfen und aktualisieren Sie regelmäßig die Risikoprofile Ihrer Anwendungen?",
+                "titleLocation": "left",
+                "description": "Der organisatorische Risikostandard berücksichtigt historisches Feedback zur Verbesserung der Bewertungsmethode",
+                "choices": [
+                    {
+                        "value": 0,
+                        "weight": 1,
+                        "text": "Nein"
+                    },
+                    {
+                        "value": 0.25,
+                        "weight": 1,
+                        "text": "Ja, sporadisch"
+                    },
+                    {
+                        "value": 0.5,
+                        "weight": 1,
+                        "text": "Ja, bei Änderung der Anwendung"
+                    },
+                    {
+                        "value": 1,
+                        "weight": 1,
+                        "text": "Ja, mindestens jährlich"
+                    }
+                ]
+            },
+            {
+                "type": "radiogroup",
+                "id": "e9dcf4f79e2e487fb74df5e15a14a91b",
+                "title": "Identifizieren und verwalten Sie Architekturmängel mit Bedrohungsmodellierung?",
+                "titleLocation": "left",
+                "description": "Sie führen eine Bedrohungsmodellierung für hochriskante Anwendungen durch",
+                "choices": [
+                    {
+                        "value": 0,
+                        "weight": 1,
+                        "text": "Nein"
+                    },
+                    {
+                        "value": 0.25,
+                        "weight": 1,
+                        "text": "Ja, einige davon"
+                    },
+                    {
+                        "value": 0.5,
+                        "weight": 1,
+                        "text": "Ja, mindestens die Hälfte davon"
+                    },
+                    {
+                        "value": 1,
+                        "weight": 1,
+                        "text": "Ja, die meisten oder alle davon"
+                    }
+                ]
+            },
+            {
+                "type": "radiogroup",
+                "id": "42cfabd13db34fd0b35e92af917eb1b8",
+                "title": "Verwenden Sie eine standardisierte Methodik, die auf Ihren Anwendungsrisikostufen basiert?",
+                "titleLocation": "left",
+                "description": "Sie schulen Ihre Architekten, Sicherheitsbeauftragten und andere Stakeholder darin, wie man praktische Bedrohungsmodellierung durchführt",
+                "choices": [
+                    {
+                        "value": 0,
+                        "weight": 1,
+                        "text": "Nein"
+                    },
+                    {
+                        "value": 0.25,
+                        "weight": 1,
+                        "text": "Ja, für einige Anwendungen"
+                    },
+                    {
+                        "value": 0.5,
+                        "weight": 1,
+                        "text": "Ja, für mindestens die Hälfte der Anwendungen"
+                    },
+                    {
+                        "value": 1,
+                        "weight": 1,
+                        "text": "Ja, für die meisten oder alle Anwendungen"
+                    }
+                ]
+            },
+            {
+                "type": "radiogroup",
+                "id": "cd639e5458ca4f60be60bc9d47314648",
+                "title": "Überprüfen und aktualisieren Sie regelmäßig die Methodik zur Bedrohungsmodellierung für Ihre Anwendungen?",
+                "titleLocation": "left",
+                "description": "Die Methodik des Bedrohungsmodells berücksichtigt historisches Feedback zur Verbesserung",
+                "choices": [
+                    {
+                        "value": 0,
+                        "weight": 1,
+                        "text": "Nein"
+                    },
+                    {
+                        "value": 0.25,
+                        "weight": 1,
+                        "text": "Ja, die Überprüfung erfolgt ad-hoc"
+                    },
+                    {
+                        "value": 0.5,
+                        "weight": 1,
+                        "text": "Ja, wir überprüfen es regelmäßig"
+                    },
+                    {
+                        "value": 1,
+                        "weight": 1,
+                        "text": "Ja, wir überprüfen es mindestens jährlich"
+                    }
+                ]
+            }
+        ],
+        "title": "Bedrohungsbeurteilung",
+        "state": "expanded"
+    });
+}
 export default threatassessment;
