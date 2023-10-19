910ffb230d680953827f22d8a8594996b3aa4597
sub-ra <87712870+sub-ra@users.noreply.github.com>
2023-10-19 16:49:09 +0200

    Update footer.js

--- comps/footer.js
+++ comps/footer.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 const Footer = () => {
     return ( 
         <footer>
-            <a href="https://owasp.org/">
-                <Image src ="/Footer.png" width = {300} height={100} />
+            <a href="https://security.migros.net/">
+                <Image src ="/Footerx.png" width = {300} height={100} />
             </a>
         </footer>
      );
