7aecb039354fd9674d2ced4ddd59bcc1f2d8db2b
sub-ra <87712870+sub-ra@users.noreply.github.com>
2023-10-19 17:44:49 +0200

    New translations archass.js (German)

--- comps/surveys/surveypanels/verification/deu_archass.js
+++ comps/surveys/surveypanels/verification/deu_archass.js
@@ -1,185 +1,185 @@
 const archass = () => {
-    return {
-        'type': 'panel',
-        'name': 'Architecture Assessment',
-        'elements': [
-            {
-                'type': 'radiogroup',
-                'id': '2da7acf355814b75ab971fec36048f11',
-                'title': 'Do you review the application architecture for key security objectives on an ad-hoc basis?',
-                'titleLocation': 'left',
-                'description': 'You have an agreed upon model of the overall software architecture',
-                'choices': [
-                    {
-                        'value': 0,
-                        'weight': 1,
-                        'text': 'No'
-                    },
-                    {
-                        'value': '0.25',
-                        'weight': 1,
-                        'text': 'Yes, for some applications'
-                    },
-                    {
-                        'value': '0.5',
-                        'weight': 1,
-                        'text': 'Yes, for at least half of the applications'
-                    },
-                    {
-                        'value': 1,
-                        'weight': 1,
-                        'text': 'Yes, for most or all of the applications'
-                    }
-                ]
-            },
-            {
-                'type': 'radiogroup',
-                'id': 'b133d28653bc47a8a8574d1c60ec34f0',
-                'title': 'Do you regularly review the security mechanisms of your architecture?',
-                'titleLocation': 'left',
-                'description': 'You review compliance with internal and external requirements',
-                'choices': [
-                    {
-                        'value': 0,
-                        'weight': 1,
-                        'text': 'No'
-                    },
-                    {
-                        'value': '0.25',
-                        'weight': 1,
-                        'text': 'Yes, for some applications'
-                    },
-                    {
-                        'value': '0.5',
-                        'weight': 1,
-                        'text': 'Yes, for at least half of the applications'
-                    },
-                    {
-                        'value': 1,
-                        'weight': 1,
-                        'text': 'Yes, for most or all of the applications'
-                    }
-                ]
-            },
-            {
-                'type': 'radiogroup',
-                'id': 'ba213b2d5fc844a386010ca53cf87fb8',
-                'title': 'Do you regularly review the effectiveness of the security controls?',
-                'titleLocation': 'left',
-                'description': 'You evaluate the preventive, detective, and response capabilities of security controls',
-                'choices': [
-                    {
-                        'value': 0,
-                        'weight': 1,
-                        'text': 'No'
-                    },
-                    {
-                        'value': '0.25',
-                        'weight': 1,
-                        'text': 'Yes, for some applications'
-                    },
-                    {
-                        'value': '0.5',
-                        'weight': 1,
-                        'text': 'Yes, for at least half of the applications'
-                    },
-                    {
-                        'value': 1,
-                        'weight': 1,
-                        'text': 'Yes, for most or all of the applications'
-                    }
-                ]
-            },
-            {
-                'type': 'radiogroup',
-                'id': 'b920062a62d84883af0c167955ec10de',
-                'title': 'Do you review the application architecture for mitigations of typical threats on an ad-hoc basis?',
-                'titleLocation': 'left',
-                'description': 'You have an agreed upon model of the overall software architecture',
-                'choices': [
-                    {
-                        'value': 0,
-                        'weight': 1,
-                        'text': 'No'
-                    },
-                    {
-                        'value': '0.25',
-                        'weight': 1,
-                        'text': 'Yes, for some applications'
-                    },
-                    {
-                        'value': '0.5',
-                        'weight': 1,
-                        'text': 'Yes, for at least half of the applications'
-                    },
-                    {
-                        'value': 1,
-                        'weight': 1,
-                        'text': 'Yes, for most or all of the applications'
-                    }
-                ]
-            },
-            {
-                'type': 'radiogroup',
-                'id': '2784c6272d174dcf932b188a69a8917d',
-                'title': 'Do you regularly evaluate the threats to your architecture?',
-                'titleLocation': 'left',
-                'description': 'You systematically review each threat identified in the Threat Assessment',
-                'choices': [
-                    {
-                        'value': 0,
-                        'weight': 1,
-                        'text': 'No'
-                    },
-                    {
-                        'value': '0.25',
-                        'weight': 1,
-                        'text': 'Yes, for some applications'
-                    },
-                    {
-                        'value': '0.5',
-                        'weight': 1,
-                        'text': 'Yes, for at least half of the applications'
-                    },
-                    {
-                        'value': 1,
-                        'weight': 1,
-                        'text': 'Yes, for most or all of the applications'
-                    }
-                ]
-            },
-            {
-                'type': 'radiogroup',
-                'id': 'df317b6ac5de4815a1ba7ac558d0263e',
-                'title': 'Do you regularly update your reference architectures based on architecture assessment findings?',
-                'titleLocation': 'left',
-                'description': 'You assess your architectures in a standardized, documented manner',
-                'choices': [
-                    {
-                        'value': 0,
-                        'weight': 1,
-                        'text': 'No'
-                    },
-                    {
-                        'value': '0.25',
-                        'weight': 1,
-                        'text': 'Yes, for some applications'
-                    },
-                    {
-                        'value': '0.5',
-                        'weight': 1,
-                        'text': 'Yes, for at least half of the applications'
-                    },
-                    {
-                        'value': 1,
-                        'weight': 1,
-                        'text': 'Yes, for most or all of the applications'
-                    }
-                ]
-            }
-        ],
-        'title': 'Architecture Assessment',
-        'state': 'expanded'
-    };
+  return {
+    'type': 'panel',
+    'name': 'Architekturbewertung',
+    'elements': [
+      {
+        'type': 'radiogroup',
+        'id': '2da7acf355814b75ab971fec36048f11',
+        'title': 'Überprüfen Sie die Anwendungsarchitektur ad hoc auf wichtige Sicherheitsziele?',
+        'titleLocation': 'left',
+        'description': 'Sie haben ein vereinbartes Modell der Gesamtsoftwarearchitektur',
+        'choices': [
+          {
+            'value': 0,
+            'weight': 1,
+            'text': 'Nein'
+          },
+          {
+            'value': '0.25',
+            'weight': 1,
+            'text': 'Ja, für einige Anwendungen'
+          },
+          {
+            'value': '0.5',
+            'weight': 1,
+            'text': 'Ja, für mindestens die Hälfte der Anwendungen'
+          },
+          {
+            'value': 1,
+            'weight': 1,
+            'text': 'Ja, für die meisten oder alle Anwendungen'
+          }
+        ]
+      },
+      {
+        'type': 'radiogroup',
+        'id': 'b133d28653bc47a8a8574d1c60ec34f0',
+        'title': 'Überprüfen Sie regelmäßig die Sicherheitsmechanismen Ihrer Architektur?',
+        'titleLocation': 'left',
+        'description': 'Sie überprüfen die Einhaltung interner und externer Anforderungen',
+        'choices': [
+          {
+            'value': 0,
+            'weight': 1,
+            'text': 'Nein'
+          },
+          {
+            'value': '0.25',
+            'weight': 1,
+            'text': 'Ja, für einige Anwendungen'
+          },
+          {
+            'value': '0.5',
+            'weight': 1,
+            'text': 'Ja, für mindestens die Hälfte der Anwendungen'
+          },
+          {
+            'value': 1,
+            'weight': 1,
+            'text': 'Ja, für die meisten oder alle Anwendungen'
+          }
+        ]
+      },
+      {
+        'type': 'radiogroup',
+        'id': 'ba213b2d5fc844a386010ca53cf87fb8',
+        'title': 'Überprüfen Sie regelmäßig die Wirksamkeit der Sicherheitskontrollen?',
+        'titleLocation': 'left',
+        'description': 'Sie bewerten die präventiven, detektiven und reaktiven Fähigkeiten der Sicherheitskontrollen',
+        'choices': [
+          {
+            'value': 0,
+            'weight': 1,
+            'text': 'Nein'
+          },
+          {
+            'value': '0.25',
+            'weight': 1,
+            'text': 'Ja, für einige Anwendungen'
+          },
+          {
+            'value': '0.5',
+            'weight': 1,
+            'text': 'Ja, für mindestens die Hälfte der Anwendungen'
+          },
+          {
+            'value': 1,
+            'weight': 1,
+            'text': 'Ja, für die meisten oder alle Anwendungen'
+          }
+        ]
+      },
+      {
+        'type': 'radiogroup',
+        'id': 'b920062a62d84883af0c167955ec10de',
+        'title': 'Überprüfen Sie die Anwendungsarchitektur ad hoc auf Maßnahmen gegen typische Bedrohungen?',
+        'titleLocation': 'left',
+        'description': 'Sie haben ein vereinbartes Modell der Gesamtsoftwarearchitektur',
+        'choices': [
+          {
+            'value': 0,
+            'weight': 1,
+            'text': 'Nein'
+          },
+          {
+            'value': '0.25',
+            'weight': 1,
+            'text': 'Ja, für einige Anwendungen'
+          },
+          {
+            'value': '0.5',
+            'weight': 1,
+            'text': 'Ja, für mindestens die Hälfte der Anwendungen'
+          },
+          {
+            'value': 1,
+            'weight': 1,
+            'text': 'Ja, für die meisten oder alle Anwendungen'
+          }
+        ]
+      },
+      {
+        'type': 'radiogroup',
+        'id': '2784c6272d174dcf932b188a69a8917d',
+        'title': 'Bewerten Sie regelmäßig die Bedrohungen für Ihre Architektur?',
+        'titleLocation': 'left',
+        'description': 'Sie überprüfen systematisch jede in der Bedrohungsanalyse identifizierte Bedrohung',
+        'choices': [
+          {
+            'value': 0,
+            'weight': 1,
+            'text': 'Nein'
+          },
+          {
+            'value': '0.25',
+            'weight': 1,
+            'text': 'Ja, für einige Anwendungen'
+          },
+          {
+            'value': '0.5',
+            'weight': 1,
+            'text': 'Ja, für mindestens die Hälfte der Anwendungen'
+          },
+          {
+            'value': 1,
+            'weight': 1,
+            'text': 'Ja, für die meisten oder alle Anwendungen'
+          }
+        ]
+      },
+      {
+        'type': 'radiogroup',
+        'id': 'df317b6ac5de4815a1ba7ac558d0263e',
+        'title': 'Aktualisieren Sie regelmäßig Ihre Referenzarchitekturen basierend auf den Erkenntnissen der Architekturbewertung?',
+        'titleLocation': 'left',
+        'description': 'Sie bewerten Ihre Architekturen in standardisierter, dokumentierter Weise',
+        'choices': [
+          {
+            'value': 0,
+            'weight': 1,
+            'text': 'Nein'
+          },
+          {
+            'value': '0.25',
+            'weight': 1,
+            'text': 'Ja, für einige Anwendungen'
+          },
+          {
+            'value': '0.5',
+            'weight': 1,
+            'text': 'Ja, für mindestens die Hälfte der Anwendungen'
+          },
+          {
+            'value': 1,
+            'weight': 1,
+            'text': 'Ja, für die meisten oder alle Anwendungen'
+          }
+        ]
+      }
+    ],
+    'title': 'Architekturbewertung',
+    'state': 'expanded'
+  };
 };
 export default archass;
