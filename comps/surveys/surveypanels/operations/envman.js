0a4343e6960f31e6fa49c5c1dabaf533d8523b34
sub-ra <87712870+sub-ra@users.noreply.github.com>
2023-10-19 16:34:40 +0200

    Update envman.js

--- comps/surveys/surveypanels/operations/envman.js
+++ comps/surveys/surveypanels/operations/envman.js
@@ -1,183 +1,186 @@
-const envman = () => {return ({
+const envman = () => {
+  return {
     "type": "panel",
     "name": "Environmental Management",
     "elements": [
       {
         "type": "radiogroup",
         "id": "1e005e11997f4929a12fdb939599e77e",
-        "title": "Do you harden configurations for key components of your technology stacks?",
+        "title": "Härten Sie Konfigurationen für Schlüsselkomponenten Ihrer Technologiestapel?",
         "titleLocation": "left",
-        "description": "You have identified the key components in each technology stack used",
+        "description": "Sie haben die Schlüsselkomponenten in jedem verwendeten Technologiestapel identifiziert",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some components"
+            "text": "Ja, für einige Komponenten"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the components"
+            "text": "Ja, für mindestens die Hälfte der Komponenten"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the components"
+            "text": "Ja, für die meisten oder alle Komponenten"
           }
         ]
       },
       {
         "type": "radiogroup",
         "id": "41d33402a94c49538554ce77e9de6a72",
-        "title": "Do you have hardening baselines for your components?",
+        "title": "Haben Sie Härtungsrichtlinien für Ihre Komponenten?",
         "titleLocation": "left",
-        "description": "You have assigned an owner for each baseline",
+        "description": "Sie haben für jede Richtlinie einen Verantwortlichen zugewiesen",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some components"
+            "text": "Ja, für einige Komponenten"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the components"
+            "text": "Ja, für mindestens die Hälfte der Komponenten"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the components"
+            "text": "Ja, für die meisten oder alle Komponenten"
           }
         ]
       },
       {
         "type": "radiogroup",
         "id": "f4ec030280ee417099eaf12752a542ae",
-        "title": "Do you monitor and enforce conformity with hardening baselines?",
+        "title": "Überwachen und erzwingen Sie regelmäßig die Konformität mit den Härtungsrichtlinien?",
         "titleLocation": "left",
-        "description": "You perform conformity checks regularly, preferably using automation",
+        "description": "Sie führen regelmäßig Konformitätsprüfungen durch, bevorzugt automatisiert",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some components"
+            "text": "Ja, für einige Komponenten"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the components"
+            "text": "Ja, für mindestens die Hälfte der Komponenten"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the components"
+            "text": "Ja, für die meisten oder alle Komponenten"
           }
         ]
       },
       {
         "type": "radiogroup",
         "id": "180e194b165d421c9d2c89258195a792",
-        "title": "Do you identify and patch vulnerable components?",
+        "title": "Identifizieren und patchen Sie verwundbare Komponenten?",
         "titleLocation": "left",
-        "description": "You have an up-to-date list of components, including version information",
+        "description": "Sie haben eine aktuelle Liste von Komponenten, einschließlich Versionsinformationen",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some components"
+            "text": "Ja, für einige Komponenten"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the components"
+            "text": "Ja, für mindestens die Hälfte der Komponenten"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the components"
+            "text": "Ja, für die meisten oder alle Komponenten"
           }
         ]
       },
       {
         "type": "radiogroup",
         "id": "0844b1a3be8b49ec83c7377a9f797cfc",
-        "title": "Do you follow an established process for updating components of your technology stacks?",
+        "title": "Folgen Sie einem etablierten Prozess für die Aktualisierung von Komponenten Ihrer Technologiestapel?",
         "titleLocation": "left",
-        "description": "The process includes vendor information for third-party patches",
+        "description": "Der Prozess enthält Informationen zum Hersteller für Patches von Drittanbietern",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some components"
+            "text": "Ja, für einige Komponenten"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the components"
+            "text": "Ja, für mindestens die Hälfte der Komponenten"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the components"
+            "text": "Ja, für die meisten oder alle Komponenten"
           }
         ]
       },
       {
         "type": "radiogroup",
         "id": "6e72179a31c04024bb649346bfb03eb5",
-        "title": "Do you regularly evaluate components and review patch level status?",
+        "title": "Evaluieren Sie regelmäßig Komponenten und überprüfen Sie den Patch-Level-Status?",
         "titleLocation": "left",
-        "description": "You update the list with components and versions",
+        "description": "Sie aktualisieren die Liste mit Komponenten und Versionen",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some components"
+            "text": "Ja, für einige Komponenten"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the components"
+            "text": "Ja, für mindestens die Hälfte der Komponenten"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the components"
+            "text": "Ja, für die meisten oder alle Komponenten"
           }
         ]
       }
     ],
     "title": "Environmental Management",
     "state": "collapsed"
-  });}
+  };
+}
+
 export default envman;
