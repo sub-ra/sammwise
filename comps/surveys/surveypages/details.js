d040c878f30b4fc7a6eb03089402e33cda340108
sub-ra <87712870+sub-ra@users.noreply.github.com>
2023-10-19 17:17:43 +0200

    Update details.js

--- comps/surveys/surveypages/details.js
+++ comps/surveys/surveypages/details.js
@@ -3,18 +3,18 @@ var tosend = {
   elements: [{
     
       "type": "text",
-      "name": "Company Name",
+      "name": "Unternehmen",
      },
      {
       "type": "text",
-      "name": "Project name"
+      "name": "Projektname"
      },
      {
       "type": "text",
-      "name": "Description of Project"
+      "name": "Projektbeschreibung"
      }
     ],
-    "description": "These are optional, the only use for these will be to help categorize the results you receive, eg: title of results document ETC "
+    "description": "Diese Angaben sind fakultativ und dienen lediglich dazu, die erhaltenen Ergebnisse zu kategorisieren, z.B.: Titel des Ergebnisdokuments etc. "
 
 }
 
