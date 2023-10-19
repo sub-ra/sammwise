76f6c6b40133494cf9c6ab69ea1296f4d4783816
sub-ra <87712870+sub-ra@users.noreply.github.com>
2023-10-19 16:35:44 +0200

    Update incman.js

--- comps/surveys/surveypanels/operations/incman.js
+++ comps/surveys/surveypanels/operations/incman.js
@@ -1,183 +1,186 @@
-const incman = () => {return ({
+const incman = () => {
+  return {
     "type": "panel",
     "name": "Incident Management",
     "elements": [
       {
         "type": "radiogroup",
         "id": "1e005e11997f4929a12fdb939599e77e",
-        "title": "Do you analyze log data for security incidents periodically?",
+        "title": "Analysieren Sie regelmäßig Logdaten auf Sicherheitsvorfälle?",
         "titleLocation": "left",
-        "description": "You have a contact point for the creation of security incidents",
+        "description": "Sie haben einen Ansprechpartner für die Erstellung von Sicherheitsvorfällen",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some applications"
+            "text": "Ja, für einige Anwendungen"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the applications"
+            "text": "Ja, für mindestens die Hälfte der Anwendungen"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the applications"
+            "text": "Ja, für die meisten oder alle Anwendungen"
           }
         ]
       },
       {
         "type": "radiogroup",
         "id": "13b9816c06444ba99584e657bfa5833d",
-        "title": "Do you follow a documented process for incident detection?",
+        "title": "Folgen Sie einem dokumentierten Prozess zur Erkennung von Vorfällen?",
         "titleLocation": "left",
-        "description": "The process has a dedicated owner",
+        "description": "Der Prozess hat einen dedizierten Verantwortlichen",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some applications"
+            "text": "Ja, für einige Anwendungen"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the applications"
+            "text": "Ja, für mindestens die Hälfte der Anwendungen"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the applications"
+            "text": "Ja, für die meisten oder alle Anwendungen"
           }
         ]
       },
       {
         "type": "radiogroup",
         "id": "09744a244f8d4076bec35130da92ea2b",
-        "title": "Do you review and update the incident detection process regularly?",
+        "title": "Überprüfen und aktualisieren Sie regelmäßig den Prozess zur Erkennung von Vorfällen?",
         "titleLocation": "left",
-        "description": "You perform reviews at least annually",
+        "description": "Sie führen mindestens einmal jährlich Überprüfungen durch",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some applications"
+            "text": "Ja, für einige Anwendungen"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the applications"
+            "text": "Ja, für mindestens die Hälfte der Anwendungen"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the applications"
+            "text": "Ja, für die meisten oder alle Anwendungen"
           }
         ]
       },
       {
         "type": "radiogroup",
         "id": "0d889a913d484eb39b80f096f3a66019",
-        "title": "Do you respond to detected incidents?",
+        "title": "Reagieren Sie auf erkannte Vorfälle?",
         "titleLocation": "left",
-        "description": "You have a defined person or role for incident handling",
+        "description": "Sie haben eine definierte Person oder Rolle für die Bearbeitung von Vorfällen",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some incidents"
+            "text": "Ja, für einige Vorfälle"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the incidents"
+            "text": "Ja, für mindestens die Hälfte der Vorfälle"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the incidents"
+            "text": "Ja, für die meisten oder alle Vorfälle"
           }
         ]
       },
       {
         "type": "radiogroup",
         "id": "dbb83b0d5b504db6a170710df4df347f",
-        "title": "Do you use a repeatable process for incident handling?",
+        "title": "Verwenden Sie einen wiederholbaren Prozess zur Bearbeitung von Vorfällen?",
         "titleLocation": "left",
-        "description": "You have an agreed upon incident classification",
+        "description": "Sie haben eine vereinbarte Einstufung von Vorfällen",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, for some incident types"
+            "text": "Ja, für einige Arten von Vorfällen"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, for at least half of the incident types"
+            "text": "Ja, für mindestens die Hälfte der Arten von Vorfällen"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, for most or all of the incident types"
+            "text": "Ja, für die meisten oder alle Arten von Vorfällen"
           }
         ]
       },
       {
         "type": "radiogroup",
         "id": "91bd2bdc1c734d8dbffc30e37158ab00",
-        "title": "Do you have a dedicated incident response team available?",
+        "title": "Haben Sie ein dediziertes Incident-Response-Team verfügbar?",
         "titleLocation": "left",
-        "description": "The team performs Root Cause Analysis for all security incidents unless there is a specific reason not to do so",
+        "description": "Das Team führt Root-Cause-Analysen für alle Sicherheitsvorfälle durch, es sei denn, es gibt einen spezifischen Grund, dies nicht zu tun",
         "choices": [
           {
             "value": 0,
             "weight": 1,
-            "text": "No"
+            "text": "Nein"
           },
           {
             "value": 0.25,
             "weight": 1,
-            "text": "Yes, some of the time"
+            "text": "Ja, manchmal"
           },
           {
             "value": 0.5,
             "weight": 1,
-            "text": "Yes, at least half of the time"
+            "text": "Ja, mindestens die Hälfte der Zeit"
           },
           {
             "value": 1,
             "weight": 1,
-            "text": "Yes, most or all of the time"
+            "text": "Ja, die meiste oder die gesamte Zeit"
           }
         ]
       }
     ],
     "title": "Incident Management",
     "state": "expanded"
-  });}
+  };
+}
+
 export default incman;
