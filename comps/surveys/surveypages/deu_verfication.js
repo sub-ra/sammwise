2a9bd11f52a2a25718cab22b25332f97d3828e90
sub-ra <87712870+sub-ra@users.noreply.github.com>
2023-10-19 14:52:32 +0200

    New translations verfication.js (German)

---
+++ comps/surveys/surveypages/deu_verfication.js
@@ -0,0 +1,16 @@
+import archass from '../surveypanels/verification/archass';
+import reqtest from '../surveypanels/verification/reqtest';
+import sectest from '../surveypanels/verification/sectest';
+//Skeleton of the Page 
+var tosend = {
+    name: 'Verification',
+    elements: [{}]
+};
+//Push elements to Skeleton 
+tosend.elements.push(archass());
+tosend.elements.push(reqtest());
+tosend.elements.push(sectest());
+const verificationJSON = () => {
+    return tosend;
+};
+export default verificationJSON;
